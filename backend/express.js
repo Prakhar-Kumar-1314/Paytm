c2766c17e80af15fd507ba0d33eb61a9ed2e1887
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2025-01-25 20:21:25 +0530

    express

--- backend/express.js
+++ backend/express.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
+const bodyParser = require("body-parser");
 
 const port = 3000;
 const app = express();
+
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use("api/v1", mainRouter);
 
