57331f87c0c2af2aeef39257418f86b5b7a39b1d
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2025-01-25 19:45:40 +0530

    added routes

--- backend/express.js
+++ backend/express.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const mainRouter = require("./routes/index");
 
 const port = 3000;
 const app = express();
 app.use(cors());
+
+app.use("api/v1", mainRouter);
 
 app.listen(() => {
   console.log(`Working on Port ${port}`);
